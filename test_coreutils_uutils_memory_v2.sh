f66132d05f1e5af262606be87528782887d2c1b4
Jonathan <68710969+Bonathan@users.noreply.github.com>
2023-08-16 13:04:23 +0200

    modified scipt 3

--- test_coreutils_uutils_memory_v2.sh
+++ test_coreutils_uutils_memory_v2.sh
@@ -2,45 +2,55 @@
 
 # Check if the correct number of arguments are provided
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <output_file>"
+    echo "Usage: $0 <output_directory>"
     exit 1
 fi
 
-# Output file to save results
-output_file="$1"
+# Output directory to save results
+output_dir="$1"
 
-# Function to run the tool and record memory usage
+# Function to run the tool and record memory usage and runtime
 run_tool() {
     local tool="$1"
     local output_file="$2"
+    local mem_file="$3"
 
-    # Run the tool using time command to capture memory usage
-    time $tool 2>&1 | tee "$output_file"
+    # Run the tool using time command to capture memory usage and runtime
+    /usr/bin/time -v $tool 2>&1 | tee "$output_file" > /dev/null
+    
+    # Extract memory usage from the output file and save it
+    memory=$(grep "Maximum resident set size (kbytes)" "$output_file" | awk '{print $NF}')
+    echo "$memory" > "$mem_file"
 }
 
 # Specify the commands for the two tools
-tool1_command="/home/bittn/.cargo/bin/coreutils ls /etc/ca-certificates/extracted/cadir"
+tool1_command="$HOME/.cargo/bin/coreutils ls /etc/ca-certificates/extracted/cadir"
 tool2_command="/usr/bin/ls /etc/ca-certificates/extracted/cadir"
 
-# Run and record memory usage of tool 1
-echo "Running tool 1 and recording memory usage..."
-run_tool "$tool1_command" "$output_file.tool1"
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
 
-# Run and record memory usage of tool 2
-echo "Running tool 2 and recording memory usage..."
-run_tool "$tool2_command" "$output_file.tool2"
+# Run and record memory usage and runtime of tool 1
+echo "Running tool 1 and recording memory usage and runtime..."
+tool1_output="$output_dir/output.tool1"
+tool1_mem="$output_dir/memory.tool1"
+run_tool "$tool1_command" "$tool1_output" "$tool1_mem"
 
-# Extract memory usage from the output files
-memory_tool1=$(grep "Maximum resident set size (kbytes)" "$output_file.tool1" | awk '{print $NF}')
-memory_tool2=$(grep "Maximum resident set size (kbytes)" "$output_file.tool2" | awk '{print $NF}')
+# Run and record memory usage and runtime of tool 2
+echo "Running tool 2 and recording memory usage and runtime..."
+tool2_output="$output_dir/output.tool2"
+tool2_mem="$output_dir/memory.tool2"
+run_tool "$tool2_command" "$tool2_output" "$tool2_mem"
+
+# Read memory usage from the memory files
+memory_tool1=$(cat "$tool1_mem")
+memory_tool2=$(cat "$tool2_mem")
 
 # Compare memory usage and print the result
-#if [ "$memory_tool1" -lt "$memory_tool2" ]; then
-#    echo "Tool 1 uses less memory than Tool 2"
-#elif [ "$memory_tool1" -gt "$memory_tool2" ]; then
-#    echo "Tool 2 uses less memory than Tool 1"
-#else
-#    echo "Tool 1 and Tool 2 use the same amount of memory"
-#fi
-
-echo $tool1_command
+if [ "$memory_tool1" -lt "$memory_tool2" ]; then
+    echo "Tool 1 uses less memory than Tool 2"
+elif [ "$memory_tool1" -gt "$memory_tool2" ]; then
+    echo "Tool 2 uses less memory than Tool 1"
+else
+    echo "Tool 1 and Tool 2 use the same amount of memory"
+fi
