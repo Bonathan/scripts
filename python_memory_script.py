3b31f0d71f2b02edf56aa16175042dafc268149f
Jonathan <68710969+Bonathan@users.noreply.github.com>
2023-08-17 11:30:23 +0200

    cycles

--- python_memory_script.py
+++ python_memory_script.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import csv
 
 def record_memory_usage(pid):
     try:
@@ -13,19 +14,22 @@ def record_memory_usage(pid):
 def main():
     cargo_command = "$HOME/.cargo/bin/coreutils ls /etc/ca-certificates/extracted/cadir"
     usr_command = "/usr/bin/ls /etc/ca-certificates/extracted/cadir"
+    cycles = 50
 
-    cargo_process = subprocess.Popen(cargo_command, shell=True)
-    usr_process = subprocess.Popen(usr_command, shell=True)
+    with open('memory_usage_output.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Cycle', 'Cargo Memory Usage (KB)', 'Usr Memory Usage (KB)'])
 
-    cargo_memory_usage = record_memory_usage(cargo_process.pid)
-    usr_memory_usage = record_memory_usage(usr_process.pid)
+        for cycle in range(cycles):
+            cargo_process = subprocess.Popen(cargo_command, shell=True)
+            usr_process = subprocess.Popen(usr_command, shell=True)
 
-    with open('memory_usage_output.csv', 'w') as file:
-        # output in KB
-        file.write(f"{cargo_memory_usage}, {usr_memory_usage}\n")
+            cargo_memory_usage = record_memory_usage(cargo_process.pid)
+            usr_memory_usage = record_memory_usage(usr_process.pid)
+
+            csvwriter.writerow([cycle + 1, cargo_memory_usage, usr_memory_usage])
 
     print("Memory usage data written to memory_usage_output.csv")
 
 if __name__ == "__main__":
-    for i in range(0,50):
-        main()
+    main()
